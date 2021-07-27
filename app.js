34b80da8a69adfc7427ce2463e4b4b45b7c6876a
ioriayaka <ioriayaka@hotmail.com>
2021-07-27 15:05:05 +0800

    feat: add mongoose and handlebars

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
+const PORT = process.env.PORT || 3000
+require('./config/mongoose')
 
-// 設定首頁路由
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-// 設定 port 3000
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
 })
