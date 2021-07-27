91b6d1a96dff845128ceea3c2e653426bb7414d5
ioriayaka <ioriayaka@hotmail.com>
2021-07-27 15:37:03 +0800

    feat: add seeder and URL function

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
+app.use(express.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   res.send('hello world')
 })
